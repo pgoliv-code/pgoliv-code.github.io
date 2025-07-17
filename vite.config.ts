4a0f6f52b80dee3c222e65f771d0bfa03cd46fa7
Priscila <pguillen.oliv@gmail.com>
2025-07-17 22:54:15 +0200

    added last fixes to package json vite config deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/pgoliv-code.github.io/',
+  base: '/',
   plugins: [react()],
 })
